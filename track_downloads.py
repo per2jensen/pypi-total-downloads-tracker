ca19bab1a668c3432e9fbc950972c3d6600e0a67
Per Jensen <per2jensen@gmail.com>
2025-04-16 17:11:20 +0200

    Update track_downloads.py
    MIT license

--- track_downloads.py
+++ track_downloads.py
@@ -2,13 +2,7 @@
 source code is here: https://github.com/per2jensen/dar-backup/blob/main/track_downloads.py
 
 
-LICENSE:
-===========
-Licensed under GNU GENERAL PUBLIC LICENSE v3, see the supplied file "LICENSE" for details.
-
-THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW, 
-not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See section 15 and section 16 in the supplied "LICENSE" file
+LICENSE:  MIT License
 """
 
 
